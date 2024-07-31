346979fba8fc6f22236583ef3effcb14c8edfbad
vedikathote <vedikathote@gmail.com>
2024-07-31 21:53:24 +0530

    added files

---
+++ script.js
@@ -0,0 +1,22 @@
+let cel=document.getElementById("cel");
+let Fah=document.getElementById("Fara");
+
+
+
+
+cel.addEventListener("input",function(){
+ var valcel=this.value;	
+let ans=(valcel * 9/5) + 32 ;
+Fah.value=ans.toFixed(2);
+
+});
+
+
+Fah.addEventListener("input",function(){
+    let valfan=this.value;
+   var anscel= (valfan- 32) * 5/9 ;
+     cel.value=anscel.toFixed(2);
+
+    
+     });
+ 
